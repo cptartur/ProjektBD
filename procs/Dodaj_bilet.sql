8f5719247986ccd51de18555733fea219497568c
David <58195838+DayWD@users.noreply.github.com>
2021-01-09 00:45:25 +0100

    Create Dodaj_bilet.sql

---
+++ procs/Dodaj_bilet.sql
@@ -0,0 +1,8 @@
+CREATE OR ALTER PROCEDURE Dodaj_bilet
+	@Id_klienta INT,
+	@Rodzaj_biletu nvarchar(50),
+	@Data_zakupu date,
+	@Id_punktu_sprzedaży INT
+AS
+	INSERT INTO Bilety
+	VALUES(@Id_klienta,@Rodzaj_biletu,-1,@Data_zakupu,@Data_zakupu,@Id_punktu_sprzedaży)
