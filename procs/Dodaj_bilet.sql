a688cdbd8cd0a8e3577a224768eb61506ed92c14
DayWD <DayWeed@github.com>
2021-01-09 14:16:26 +0100

    Update Dodaj_bilet.sql

--- procs/Dodaj_bilet.sql
+++ procs/Dodaj_bilet.sql
@@ -4,5 +4,23 @@ CREATE OR ALTER PROCEDURE Dodaj_bilet
 	@Data_zakupu date,
 	@Id_punktu_sprzedaży INT
 AS
-	INSERT INTO Bilety
-	VALUES(@Id_klienta,@Rodzaj_biletu,-1,@Data_zakupu,@Data_zakupu,@Id_punktu_sprzedaży)
+	IF NOT EXISTS (
+					SELECT * 
+					FROM Punkty_sprzedaży
+					WHERE ID_punktu = @Id_punktu_sprzedaży)
+	BEGIN
+		RAISERROR('Nie ma takiego punktu sprzedaży',16,1)
+		RETURN;
+	END
+	ELSE IF NOT EXISTS (
+					SELECT * 
+					FROM Klienci
+					WHERE ID_klienta = @Id_klienta)
+	BEGIN
+		RAISERROR('Nie ma takiego klienta',16,2)
+	END
+	ELSE
+	BEGIN
+		INSERT INTO Bilety
+		VALUES(@Id_klienta,@Rodzaj_biletu,-1,@Data_zakupu,@Data_zakupu,@Id_punktu_sprzedaży)
+	END
