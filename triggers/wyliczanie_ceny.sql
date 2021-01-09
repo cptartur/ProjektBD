450628d517278028bb33ae1bb97daccb8a25c0e5
David <58195838+DayWD@users.noreply.github.com>
2021-01-09 12:47:29 +0100

    Update wyliczanie_ceny.sql

--- triggers/wyliczanie_ceny.sql
+++ triggers/wyliczanie_ceny.sql
@@ -1,10 +1,15 @@
 CREATE OR ALTER TRIGGER wyliczanie_ceny ON Bilety
 AFTER INSERT
 AS
-	UPDATE Bilety
-	SET Cena = (SELECT ROUND(Cena_bazowa*CAST((1-U.Zniżka/100.0) as MONEY),2) from Rodzaje_biletów as R
-				JOIN Bilety as B on R.Rodzaj_biletu = B.Rodzaj_biletu
-				JOIN Klienci as K on B.ID_klienta = K.ID_klienta
-				JOIN Ulgi as U on K.Ulga = U.Rodzaj_ulgi
-				WHERE B.ID_biletu = (SELECT Max(ID_biletu) FROM Bilety) )
-	WHERE ID_biletu = (SELECT Max(ID_biletu) FROM Bilety)
+
+UPDATE Bilety
+SET cena = (CASE WHEN Ulga IS NOT NULL THEN (ROUND(Cena_bazowa*CAST((1-U.Zniżka/100.0) as MONEY),2)) ELSE R.Cena_bazowa END)
+FROM Bilety AS B
+INNER JOIN Rodzaje_biletów AS R
+    ON R.Rodzaj_biletu = B.Rodzaj_biletu
+INNER JOIN Klienci AS K
+    ON B.ID_klienta = K.ID_klienta
+INNER JOIN Ulgi AS U
+    ON U.Rodzaj_ulgi = K.Ulga
+INNER JOIN inserted AS I
+    ON I.ID_biletu = B.ID_biletu
