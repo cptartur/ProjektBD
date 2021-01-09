2d161c1ff4e0f1ddaba9c6407b2cfa16c952ae67
David <58195838+DayWD@users.noreply.github.com>
2021-01-09 12:16:14 +0100

    Update wyliczanie_daty_wygasniecia_biletu.sql

--- triggers/wyliczanie_daty_wygasniecia_biletu.sql
+++ triggers/wyliczanie_daty_wygasniecia_biletu.sql
@@ -2,9 +2,7 @@ CREATE OR ALTER TRIGGER wyliczanie_daty_wygasniecia_biletu ON Bilety
 AFTER INSERT
 AS
 	UPDATE Bilety
-	SET Data_wygaśnięcia = (SELECT DATEADD(DD,Okres_w_dniach,Data_zakupu) from Rodzaje_biletów as R
-				JOIN Bilety as B on R.Rodzaj_biletu = B.Rodzaj_biletu
-				JOIN Klienci as K on B.ID_klienta = K.ID_klienta
-				JOIN Ulgi as U on K.Ulga = U.Rodzaj_ulgi
-				WHERE B.ID_biletu = (SELECT Max(ID_biletu) FROM Bilety) )
-	WHERE ID_biletu = (SELECT Max(ID_biletu) FROM Bilety)
+	SET Data_wygaśnięcia = DATEADD (DD, R.Okres_w_dniach, I.Data_zakupu)
+	FROM inserted as I
+	JOIN Rodzaje_biletów AS R
+    ON I.Rodzaj_biletu = R.Rodzaj_biletu
