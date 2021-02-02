423e65863f6ca6ee10f08ab95c7776a4953468a7
Artur Michalek <artumi1@gmail.com>
2021-02-02 15:08:16 +0100

    Rename

--- views/wynagrodzenia_view.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR ALTER VIEW Wynagrodzenia
-AS
-SELECT C.ID_kierowcy, K.Imiê, C.Rok, C.Miesi¹c, K.Nazwisko, (
-	CAST((C.Godziny_pracy * 10 + C.Nadgodziny * 17) AS money)
-) AS Pensja
-FROM Czas_pracy C
-INNER JOIN Kierowcy K
-	ON K.ID_kierowcy = C.ID_kierowcy
